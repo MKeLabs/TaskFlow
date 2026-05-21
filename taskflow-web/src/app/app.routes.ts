49fd79f4f19715ea4d1aa99b9136508901a8fda6
Mike <mihaigurau97@gmail.com>
2026-05-21 20:48:25 +0300

    Added projects management route protection for admin roles only

--- taskflow-web/src/app/app.routes.ts
+++ taskflow-web/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/auth/auth.guard';
+import { adminGuard } from './core/auth/admin.guard';
 import { LoginComponent } from './pages/login/login.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { TasksComponent } from './pages/tasks/tasks.component';
@@ -9,7 +10,7 @@ import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.compone
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'projects' },
   { path: 'login', component: LoginComponent },
-  { path: 'projects', component: ProjectsComponent, canActivate: [authGuard] },
+  { path: 'projects', component: ProjectsComponent, canActivate: [adminGuard] },
   { path: 'tasks', component: TasksComponent, canActivate: [authGuard] },
   { path: 'tasks/:id', component: TaskDetailComponent, canActivate: [authGuard] },
   { path: 'unauthorized', component: UnauthorizedComponent },
